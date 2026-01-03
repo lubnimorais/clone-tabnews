909a8c08f962760243c06972f7e8607ba69e60d0
Lubni Morais <lubni.morais@gmail.com>
2026-01-03 11:51:33 -0300

    frase

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,11 @@
 export default function Home() {
-  return <h1>Hello World</h1>
+  return (
+    <div>
+      <h1>
+        "A compaixão pelos animais está intimamente ligada à bondade de caráter, 
+        e pode-se afirmar com confiança que quem é cruel com os animais não pode ser um bom homem."</h1>
+
+      <h2>Arthur Schopenhauer</h2>
+    </div>
+  )
 }
