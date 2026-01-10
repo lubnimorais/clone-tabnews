eb88ac6d00c7238c2ad69b073b33b77c5a47ad0a
Lubni Morais <lubni.morais@gmail.com>
2026-01-10 13:06:48 -0300

    adiciona scripts `lint:check` e `link:fix`

--- pages/index.js
+++ pages/index.js
@@ -2,10 +2,12 @@ export default function Home() {
   return (
     <div>
       <h1>
-        "A compaixão pelos animais está intimamente ligada à bondade de caráter 
-        e pode-se afirmar com confiança que quem é cruel com os animais não pode ser um bom homem."</h1>
+        "A compaixão pelos animais está intimamente ligada à bondade de caráter
+        e pode-se afirmar com confiança que quem é cruel com os animais não pode
+        ser um bom homem."
+      </h1>
 
       <h2>Arthur Schopenhauer</h2>
     </div>
-  )
+  );
 }
