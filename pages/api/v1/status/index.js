2f029006a6dad5d874aa4e1bfd3ff3a38227e892
Lubni Morais <lubni.morais@gmail.com>
2026-01-31 14:16:21 -0300

    add `database.js` and `.env.example` files

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,4 +1,8 @@
-export default function status(request, response) {
+import database from "../../../../infra/database.js";
+
+export default async function status(request, response) {
+  const result = await database.query("SELECT 1 + 1;");
+
   response
     .status(200)
     .json({ message: "alunos do curso.dev são pessoas acima da media" });
