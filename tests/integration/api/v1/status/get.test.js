7fd3a5c788d4b661371b175d1bdb40212279f41c
Lubni Morais <lubni.morais@gmail.com>
2026-02-18 15:38:59 -0300

    adds `Automated Tests` Github Action

--- tests/integration/api/v1/status/get.test.js
+++ tests/integration/api/v1/status/get.test.js
@@ -14,7 +14,7 @@ describe("GET to /api/v1/status", () => {
     const parsedUpdatedAt = new Date(responseBody.updated_at).toISOString();
     expect(responseBody.updated_at).toEqual(parsedUpdatedAt);
 
-    expect(responseBody.dependencies.database.version).toEqual("16.x");
+    expect(responseBody.dependencies.database.version).toEqual("16.11");
     expect(responseBody.dependencies.database.max_connections).toEqual(100);
     expect(responseBody.dependencies.database.opened_connections).toEqual(1);
   });
