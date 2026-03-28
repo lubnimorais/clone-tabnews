6f74b03d265223e65aeed004689356cfcd266569
Lubni Morais <lubni.morais@gmail.com>
2026-03-28 16:41:05 -0300

    feat: use `ServiceError` in `database.js`

--- infra/database.js
+++ infra/database.js
@@ -1,5 +1,7 @@
 import { Client } from "pg";
 
+import { ServiceError } from "./errors";
+
 async function query(queryObject) {
   let client;
 
@@ -9,9 +11,11 @@ async function query(queryObject) {
 
     return result;
   } catch (error) {
-    console.log("\n Erro dentro do catch do database.js:");
-    console.error(error);
-    throw error;
+    const serviceErrorObject = new ServiceError({
+      cause: error,
+      message: "Erro na conexão com o Banco ou na Query.",
+    });
+    throw serviceErrorObject;
   } finally {
     await client?.end();
   }
