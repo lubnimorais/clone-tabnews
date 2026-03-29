fbafb2364f834bd132ddbad21e31f69977471c24
Lubni Morais <lubni.morais@gmail.com>
2026-03-29 15:06:07 -0300

    feat: add `runPengingMigrations` method to `orchestrator`

--- tests/orchestrator.js
+++ tests/orchestrator.js
@@ -2,6 +2,8 @@ import retry from "async-retry";
 
 import database from "infra/database";
 
+import migrator from "models/migrator";
+
 // AGUARDAR POR TODOS OS SERVIÇOS ESTAREM PRONTOS
 async function waitForServices() {
   await waitForWebServer();
@@ -36,9 +38,14 @@ async function clearDatabase() {
   await database.query("drop schema public cascade; create schema public;");
 }
 
+async function runPendingMigrations() {
+  await migrator.runPendingMigrations();
+}
+
 const orchestrator = {
   waitForServices,
   clearDatabase,
+  runPendingMigrations,
 };
 
 export default orchestrator;
