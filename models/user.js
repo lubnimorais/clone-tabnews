d20fc4beb092e074c0ccaa916ce92063fca3f164
Lubni Morais <lubni.morais@gmail.com>
2026-04-11 14:21:21 -0300

    feat: hash `password` in `user.create`

--- models/user.js
+++ models/user.js
@@ -1,4 +1,7 @@
 import database from "infra/database";
+
+import password from "models/password.js";
+
 import { NotFoundError, ValidationError } from "infra/errors";
 
 async function findOneByUsername(username) {
@@ -36,6 +39,8 @@ async function create(userInputValues) {
   await validateUniqueEmail(userInputValues.email);
 
   await validateUniqueUsername(userInputValues.username);
+
+  await hashPasswordInObject(userInputValues);
 
   const newUser = await runInsertQuery(userInputValues);
 
@@ -81,6 +86,12 @@ async function create(userInputValues) {
         action: "Utilize outro username para realizar o cadastro.",
       });
     }
+  }
+
+  async function hashPasswordInObject(userInputValues) {
+    const hashedPassword = await password.hash(userInputValues.password);
+
+    userInputValues.password = hashedPassword;
   }
 
   async function runInsertQuery(userInputValues) {
--- tests/integration/api/v1/users/post.test.js
+++ tests/integration/api/v1/users/post.test.js
@@ -2,6 +2,10 @@ import { version as uuidVersion } from "uuid";
 
 import orchestrator from "tests/orchestrator";
 
+import user from "models/user.js";
+
+import password from "models/password.js";
+
 beforeAll(async () => {
   await orchestrator.waitForServices();
   await orchestrator.clearDatabase();
@@ -31,7 +35,7 @@ describe("POST /api/v1/users", () => {
         id: responseBody.id,
         username: "johndoe",
         email: "johndoe@test.com",
-        password: "senha123",
+        password: responseBody.password,
         created_at: responseBody.created_at,
         updated_at: responseBody.updated_at,
       });
@@ -39,6 +43,21 @@ describe("POST /api/v1/users", () => {
       expect(uuidVersion(responseBody.id)).toBe(4);
       expect(Date.parse(responseBody.created_at)).not.toBeNaN();
       expect(Date.parse(responseBody.updated_at)).not.toBeNaN();
+
+      const userInDatabase = await user.findOneByUsername("johndoe");
+
+      const correctPasswordMatch = await password.compare(
+        "senha123",
+        userInDatabase.password,
+      );
+
+      const inCorrectPasswordMatch = await password.compare(
+        "SenhaErrada",
+        userInDatabase.password,
+      );
+
+      expect(correctPasswordMatch).toBe(true);
+      expect(inCorrectPasswordMatch).toBe(false);
     });
 
     test("With duplicated 'email'", async () => {
