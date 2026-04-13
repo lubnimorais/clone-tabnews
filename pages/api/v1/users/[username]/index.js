a465e334ea7204758f392aa8fe1588765084a93d
Lubni Morais <lubni.morais@gmail.com>
2026-04-13 09:52:21 -0300

    feat: add support for PATCH on /api/v1/users/[username]

--- pages/api/v1/users/[username]/index.js
+++ pages/api/v1/users/[username]/index.js
@@ -7,6 +7,7 @@ import user from "models/user";
 const router = createRouter();
 
 router.get(getHandler);
+router.patch(patchHandler);
 
 export default router.handler(controller.errorHandlers);
 
@@ -16,4 +17,14 @@ async function getHandler(request, response) {
   const userFound = await user.findOneByUsername(username);
 
   return response.status(200).json(userFound);
+}
+
+async function patchHandler(request, response) {
+  const username = request.query.username;
+
+  const userInputValues = request.body;
+
+  const updatedUser = await user.update(username, userInputValues);
+
+  return response.status(200).json(updatedUser);
 }
