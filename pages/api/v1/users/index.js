da0c23a6486ed60c5f2fa2fdea0a895b86468c63
Lubni Morais <lubni.morais@gmail.com>
2026-05-04 12:04:02 -0300

    feat: send activation email after `user` registration

--- pages/api/v1/users/index.js
+++ pages/api/v1/users/index.js
@@ -3,6 +3,7 @@ import { createRouter } from "next-connect";
 import controller from "infra/controller";
 
 import user from "models/user";
+import activation from "models/activation";
 
 const router = createRouter();
 
@@ -14,6 +15,9 @@ async function postHandler(request, response) {
   const userInputValues = request.body;
 
   const newUser = await user.create(userInputValues);
+
+  const activationToken = await activation.create(newUser.id);
+  await activation.sendEmailToUser(newUser, activationToken);
 
   return response.status(201).json(newUser);
 }
